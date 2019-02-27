aa81625d9ebba7c58e4d7b375f00c3c5a38b7412
Jan Lepper <jan_lepper@gmx.de>
2019-02-27 16:51:34 +0100

    g_log: use cout instead of cerr

--- core_wrapper/main/main.cpp
+++ core_wrapper/main/main.cpp
@@ -363,7 +363,7 @@ void WINAPI il2ge_init()
 
   ofstream log(g_log_file_name);
 
-  g_log.m_outputs.push_back(&cerr);
+  g_log.m_outputs.push_back(&cout);
   g_log.m_outputs.push_back(&log);
 
   g_log.printSeparator();
